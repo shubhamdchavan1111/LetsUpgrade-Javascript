03887363496843068f2a85bf377c16c65cab61f8
shubham chavan <66768929+shubhamdchavan1111@users.noreply.github.com>
2020-07-18 05:25:35 -0700

    Add files via upload

---
+++ Day1/index.js
@@ -0,0 +1,23 @@
+// here is the explanation of the first question
+
+// console.log() method 
+console.log('abc'); 
+console.log(1); 
+console.log(true); 
+console.log(null); 
+console.log(undefined); 
+console.log([1, 2, 3, 4]); // array inside log 
+console.log({a:1, b:2, c:3}); // object inside log 
+
+// console.error() method 
+console.error('This is a simple error'); 
+
+// console.warn() method 
+console.warn('This is a warning.'); 
+
+// console.clear() method 
+console.clear(); 
+
+// console.table() method 
+console.table({'a':1, 'b':2}); 
+
